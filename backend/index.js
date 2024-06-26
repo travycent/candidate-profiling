bd36905b7a0dc7c79b78f378b6af22a067784b2f
Travy Cent <centtravy@gmail.com>
2024-06-26 15:18:52 +0300

    Fixed Cors errors

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,31 @@ import {PORT} from "./config/config.js";
 import candidateRoutes from './routes/candidateRoutes.js';
 import { setUserDAO } from './controllers/candidateController.js';
 import MySQLUserDAO from './dao/mysqlUserDao.js';
+import cors from 'cors';
+// import cache from "express-cache-memory"
 
 const app = express();
 
 //Allow usage for Json 
 app.use(express.json());
+
+//config cors
+app.use(
+    cors({
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
+
+// Configure cache middleware with options
+// const cacheMiddleware = cache({
+//     maxAge: 60000, 
+//     statusCodes: [200] 
+//   });
+
+// // Apply cache middleware to all routes
+// app.use(cacheMiddleware); 
 
 // Set the DAO implementation
 setUserDAO(new MySQLUserDAO());
